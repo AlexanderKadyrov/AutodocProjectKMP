cbc4e3faa7ebda1c1bc578ab387837eb9f015894
Alexander K <alexander-kad@yandex.ru>
2026-06-20 23:33:30 +0500

    fixes

--- sharedLogic/build.gradle.kts
+++ sharedLogic/build.gradle.kts
@@ -38,6 +38,12 @@ kotlin {
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
         }
+        androidMain.dependencies {
+            implementation(libs.ktor.client.okhttp)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
