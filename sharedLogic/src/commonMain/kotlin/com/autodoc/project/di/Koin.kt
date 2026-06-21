6ec4f6e5a46a1874d37a25bec7ee6cb579d4d39f
Alexander K <alexander-kad@yandex.ru>
2026-06-22 02:11:32 +0500

    fixes

--- sharedLogic/src/commonMain/kotlin/com/autodoc/project/di/Koin.kt
+++ sharedLogic/src/commonMain/kotlin/com/autodoc/project/di/Koin.kt
@@ -1,8 +1,8 @@
 package com.autodoc.project.di
 
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import com.autodoc.project.services.news.NewsService
 import com.autodoc.project.services.news.NewsServiceImpl
+import com.autodoc.project.services.news.NewsService
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.core.context.startKoin
