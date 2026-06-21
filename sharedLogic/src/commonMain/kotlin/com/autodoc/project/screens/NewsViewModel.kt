1b2e853700916381ed1a4822de6124cfb1e5cc75
Alexander K <alexander-kad@yandex.ru>
2026-06-22 02:24:11 +0500

    fixes

--- sharedLogic/src/commonMain/kotlin/com/autodoc/project/screens/NewsViewModel.kt
+++ sharedLogic/src/commonMain/kotlin/com/autodoc/project/screens/NewsViewModel.kt
@@ -1,8 +1,21 @@
 package com.autodoc.project.screens
 
+import com.rickclephas.kmp.nativecoroutines.NativeCoroutinesState
 import com.autodoc.project.services.news.NewsRepository
+import com.autodoc.project.services.news.NewsModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 
 class NewsViewModel {
 
     private val repository = NewsRepository()
+
+    @NativeCoroutinesState
+    val news: StateFlow<List<NewsModel>> = repository.news
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            emptyList()
+        )
 }
