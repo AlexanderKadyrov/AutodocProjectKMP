cf89022b8d8af252f634ab52f8af57ff0940b3bd
Alexander K <alexander-kad@yandex.ru>
2026-06-22 19:02:55 +0500

    fixes

--- androidApp/src/main/kotlin/com/autodoc/project/App.kt
+++ androidApp/src/main/kotlin/com/autodoc/project/App.kt
@@ -3,9 +3,14 @@ package com.autodoc.project
 import androidx.navigation.compose.rememberNavController
 import androidx.compose.material3.MaterialTheme
 import androidx.navigation.compose.composable
-import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
 import androidx.compose.material3.Surface
+
 import kotlinx.serialization.Serializable
 
 import com.autodoc.project.screens.ContentView
@@ -13,14 +18,25 @@ import com.autodoc.project.screens.ContentView
 @Serializable
 object ListDestination
 
+@Serializable
+object DetailDestination
+
 @Composable
 fun App() {
-    MaterialTheme {
+    MaterialTheme(
+        colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+    ) {
         Surface {
             val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = ListDestination) {
+            NavHost(
+                navController = navController,
+                startDestination = ListDestination
+            ) {
                 composable<ListDestination> {
                     ContentView()
+                }
+                composable<DetailDestination> {
+
                 }
             }
         }
