9c50d23bbdeda59bb770e34c18af7e01099eece2
Alexander K <alexander-kad@yandex.ru>
2026-06-22 19:31:33 +0500

    fixes

--- androidApp/src/main/kotlin/com/autodoc/project/MainActivity.kt
+++ androidApp/src/main/kotlin/com/autodoc/project/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.autodoc.project
 
-import android.os.Bundle
+import androidx.navigation.compose.rememberNavController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+
+import android.os.Bundle
 
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            App()
+            val navController = rememberNavController()
+            App(navController)
         }
     }
 }
