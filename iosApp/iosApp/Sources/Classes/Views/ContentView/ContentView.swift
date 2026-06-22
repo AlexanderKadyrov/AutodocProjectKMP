956f9962341690f3883a713a3ae1da5415498e56
Alexander K <alexander-kad@yandex.ru>
2026-06-22 15:01:57 +0500

    fixes

--- iosApp/iosApp/Sources/Classes/Views/ContentView/ContentView.swift
+++ iosApp/iosApp/Sources/Classes/Views/ContentView/ContentView.swift
@@ -5,27 +5,37 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @Environment(\.openURL) private var openURL
-
     @StateViewModel
     private var viewModel = NewsViewModel()
-
+    
+    @State
+    private var path: [Route] = []
+    
     var body: some View {
-        List {
-            ForEach(viewModel.news, id: \.self) { model in
-                NewsComponentView(model: model)
-                    .listRowInsets(.init(.zero))
-                    .listRowSeparator(.hidden)
-                    .padding(.all, 16)
-                    .onTapGesture {
-                        guard let url = URL(string: model.fullUrl) else { return }
-                        openURL(url)
-                    }
+        NavigationStack(path: $path) {
+            List {
+                ForEach(viewModel.news, id: \.self) { model in
+                    NewsComponentView(model: model)
+                        .listRowInsets(.init(.zero))
+                        .listRowSeparator(.hidden)
+                        .padding(.all, 16)
+                        .onTapGesture {
+                            guard let url = URL(string: model.fullUrl) else { return }
+                            path.append(.detail(url))
+                        }
+                }
             }
-        }
-        .listStyle(.plain)
-        .onAppear {
-            viewModel.fetchNews(offset: 1, limit: 15)
+            .listStyle(.plain)
+            .onAppear {
+                viewModel.fetchNews(offset: 1, limit: 15)
+            }
+            .navigationTitle("News")
+            .navigationDestination(for: Route.self) { route in
+                switch route {
+                case .detail(let url):
+                    LegacyWebView(url: url)
+                }
+            }
         }
     }
 }
