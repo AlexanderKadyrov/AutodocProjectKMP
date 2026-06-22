bad35dcaf9b59e7c224465b5f4e5b80b449bb79a
Alexander K <alexander-kad@yandex.ru>
2026-06-22 19:04:18 +0500

    fixes

--- androidApp/src/main/kotlin/com/autodoc/project/MainActivity.kt
+++ androidApp/src/main/kotlin/com/autodoc/project/MainActivity.kt
@@ -5,11 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
-class MainActivity : ComponentActivity() {
+class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             App()
         }
