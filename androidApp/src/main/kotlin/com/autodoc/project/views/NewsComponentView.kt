9cdae49dce92caaefb0c78122b52a8b07c08703e
Alexander K <alexander-kad@yandex.ru>
2026-06-25 00:25:38 +0500

    fixes

--- androidApp/src/main/kotlin/com/autodoc/project/views/NewsComponentView.kt
+++ androidApp/src/main/kotlin/com/autodoc/project/views/NewsComponentView.kt
@@ -5,10 +5,13 @@ import com.autodoc.project.services.news.NewsModel
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun NewsComponentView(model: NewsModel) {
@@ -18,8 +21,27 @@ fun NewsComponentView(model: NewsModel) {
                 modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                Text(model.title)
-                Text(model.descriptions)
+                Text(
+                    text = model.title,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = model.descriptions,
+                    fontSize = 14.sp,
+                )
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Text(
+                        text = model.publishedDate,
+                        fontSize = 12.sp
+                    )
+                    Text(
+                        text = model.categoryType,
+                        fontSize = 12.sp
+                    )
+                }
             }
         }
     }
