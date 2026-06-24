20c75b8fec98f21e97c8bdaab0a9cccdc9f0a5bc
Alexander K <alexander-kad@yandex.ru>
2026-06-25 00:48:00 +0500

    fixes

--- androidApp/src/main/kotlin/com/autodoc/project/views/ContentView.kt
+++ androidApp/src/main/kotlin/com/autodoc/project/views/ContentView.kt
@@ -1,7 +1,6 @@
 package com.autodoc.project.views
 
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.runtime.Composable
 
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -9,11 +8,9 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 
 import com.autodoc.project.screens.NewsViewModel
 
@@ -24,17 +21,11 @@ fun ContentView() {
         viewModel.fetchNews(1, 15)
     }
     val news by viewModel.news.collectAsStateWithLifecycle()
-    AnimatedContent(news.isNotEmpty()) { isAvailableContent ->
-        if (isAvailableContent) {
-            LazyColumn(
-                contentPadding = WindowInsets.safeDrawing.asPaddingValues()
-            ) {
-                items(news) { model ->
-                    NewsComponentView(model)
-                }
-            }
-        } else {
-            ContentEmptyView(Modifier.fillMaxSize())
+    LazyColumn(
+        contentPadding = WindowInsets.safeDrawing.asPaddingValues()
+    ) {
+        items(news) { model ->
+            NewsComponentView(model)
         }
     }
 }
