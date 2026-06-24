34e03ae4f7f19b3d2cf01c953893a9600ea95ae6
Alexander K <alexander-kad@yandex.ru>
2026-06-24 23:42:14 +0500

    fixes

--- androidApp/src/main/kotlin/com/autodoc/project/views/ContentView.kt
+++ androidApp/src/main/kotlin/com/autodoc/project/views/ContentView.kt
@@ -1,8 +1,38 @@
 package com.autodoc.project.views
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+
+import com.autodoc.project.screens.NewsViewModel
 
 @Composable
 fun ContentView() {
-
+    val viewModel = NewsViewModel()
+    val news by viewModel.news.collectAsStateWithLifecycle()
+    AnimatedContent(news.isNotEmpty()) { isAvailableContent ->
+        if (isAvailableContent) {
+            LazyColumn(
+                contentPadding = WindowInsets.safeDrawing.asPaddingValues()
+            ) {
+                items(news) { model ->
+                    Text(model.title)
+                }
+            }
+        } else {
+            ContentEmptyView(Modifier.fillMaxSize())
+        }
+    }
+    viewModel.fetchNews(1, 15)
 }
