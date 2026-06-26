8b59b10a708089c6e5402a99c685a1bbc194306f
Alexander K <alexander-kad@yandex.ru>
2026-06-26 14:18:36 +0500

    fixes

--- androidApp/src/main/kotlin/com/autodoc/project/App.kt
+++ androidApp/src/main/kotlin/com/autodoc/project/App.kt
@@ -1,20 +1,14 @@
 package com.autodoc.project
 
-import androidx.compose.material3.MaterialTheme
 import androidx.navigation.compose.composable
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.material3.Surface
 import androidx.navigation.toRoute
 
 import kotlinx.serialization.Serializable
 
 import com.autodoc.project.views.NavigationTopAppBarBar
+import com.autodoc.project.views.NavigationHost
 import com.autodoc.project.views.CustomWebView
 import com.autodoc.project.views.NewsListView
 
@@ -22,7 +16,7 @@ import com.autodoc.project.views.NewsListView
 object NewsListView
 
 @Serializable
-data class DetailDestination(
+data class NewsDetailView(
     val url: String
 )
 
@@ -30,28 +24,22 @@ data class DetailDestination(
 fun App(
     navController: NavHostController
 ) {
-    MaterialTheme(
-        colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+    NavigationHost(
+        navController = navController,
+        startDestination = NewsListView
     ) {
-        Surface {
-            NavHost(
-                navController = navController,
-                startDestination = NewsListView
-            ) {
-                composable<NewsListView> {
-                    NewsListView { url ->
-                        navController.navigate(DetailDestination(url))
-                    }
-                }
-                composable<DetailDestination> { backStackEntry ->
-                    NavigationTopAppBarBar(
-                        onBackClick = {
-                            navController.popBackStack()
-                        }) {
-                        val url = backStackEntry.toRoute<DetailDestination>().url
-                        CustomWebView(url)
-                    }
-                }
+        composable<NewsListView> {
+            NewsListView { url ->
+                navController.navigate(NewsDetailView(url))
+            }
+        }
+        composable<NewsDetailView> { backStackEntry ->
+            NavigationTopAppBarBar(
+                onBackClick = {
+                    navController.popBackStack()
+                }) {
+                val destination = backStackEntry.toRoute<NewsDetailView>()
+                CustomWebView(destination.url)
             }
         }
     }
