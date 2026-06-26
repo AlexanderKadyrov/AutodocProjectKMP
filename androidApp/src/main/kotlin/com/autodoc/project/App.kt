dd6964059a05e2a83451ed46156ea85d18cdab75
Alexander K <alexander-kad@yandex.ru>
2026-06-26 14:04:43 +0500

    fixes

--- androidApp/src/main/kotlin/com/autodoc/project/App.kt
+++ androidApp/src/main/kotlin/com/autodoc/project/App.kt
@@ -14,6 +14,7 @@ import androidx.navigation.toRoute
 
 import kotlinx.serialization.Serializable
 
+import com.autodoc.project.views.NavigationTopAppBarBar
 import com.autodoc.project.views.CustomWebView
 import com.autodoc.project.views.NewsListView
 
@@ -43,8 +44,13 @@ fun App(
                     }
                 }
                 composable<DetailDestination> { backStackEntry ->
-                    val url = backStackEntry.toRoute<DetailDestination>().url
-                    CustomWebView(url)
+                    NavigationTopAppBarBar(
+                        onBackClick = {
+                            navController.popBackStack()
+                        }) {
+                        val url = backStackEntry.toRoute<DetailDestination>().url
+                        CustomWebView(url)
+                    }
                 }
             }
         }
