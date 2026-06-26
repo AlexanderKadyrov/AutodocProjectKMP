5da4305ec5be75ac2bfa015d2be6179292dae373
Alexander K <alexander-kad@yandex.ru>
2026-06-26 14:03:30 +0500

    fixes

---
+++ androidApp/src/main/kotlin/com/autodoc/project/views/NavigationTopAppBarBar.kt
@@ -0,0 +1,37 @@
+package com.autodoc.project.views
+
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Icon
+import androidx.compose.ui.Modifier
+
+@Composable
+public fun NavigationTopAppBarBar(
+    onBackClick: () -> Unit,
+    content: @Composable ((PaddingValues) -> Unit)
+): Unit {
+    Scaffold(
+        topBar = {
+            @OptIn(ExperimentalMaterial3Api::class)
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    IconButton(onClick = onBackClick) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
+                    }
+                }
+            )
+        },
+        modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars),
+        content = content
+    )
+}
