deda2711e00192a8657d111bdb1186a2ae9e3f60
Alexander K <alexander-kad@yandex.ru>
2026-06-29 15:30:52 +0500

    fixes

--- sharedLogic/src/commonMain/kotlin/com/autodoc/project/services/news/NewsModel.kt
+++ sharedLogic/src/commonMain/kotlin/com/autodoc/project/services/news/NewsModel.kt
@@ -1,5 +1,9 @@
 package com.autodoc.project.services.news
 
+import kotlinx.datetime.format.FormatStringsInDatetimeFormats
+import kotlinx.datetime.format.byUnicodePattern
+import kotlinx.datetime.LocalDateTime
+
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
 
@@ -16,4 +20,12 @@ data class NewsModel(
     val fullUrl: String,
     val titleImageUrl: String? = null,
     val categoryType: String
-)
+) {
+    @OptIn(FormatStringsInDatetimeFormats::class)
+    fun formattedDate(): String {
+        val inputFormat = LocalDateTime.Format { byUnicodePattern("yyyy-MM-dd HH:mm:ss") }
+        val outputFormat = LocalDateTime.Format { byUnicodePattern("dd MMMM yyyy") }
+        val dateTime = inputFormat.parse(publishedDate)
+        return outputFormat.format(dateTime)
+    }
+}
