9a733e17466267c08c2e3ae22154a2a2b44bcc1f
Alexander K <alexander-kad@yandex.ru>
2026-06-29 18:51:19 +0500

    fixes

--- iosApp/iosApp/Sources/Classes/Views/News/NewsComponentView.swift
+++ iosApp/iosApp/Sources/Classes/Views/News/NewsComponentView.swift
@@ -22,7 +22,7 @@ struct NewsComponentView: View {
                     
                     HStack(alignment: .center, spacing: 16) {
                         
-                        Text(model.formattedDate())
+                        Text(model.publishedDate)
                             .frame(maxWidth: .infinity, alignment: .leading)
                             .font(Font.system(size: 12))
                         
