1c5f5d266f0f8e8f7119f0cb575b1af9c279d2fa
Alexander K <alexander-kad@yandex.ru>
2026-07-03 01:22:19 +0500

    fixes

--- sharedLogic/src/androidMain/kotlin/com/autodoc/project/database/Database.kt
+++ sharedLogic/src/androidMain/kotlin/com/autodoc/project/database/Database.kt
@@ -6,7 +6,6 @@ import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import kotlinx.coroutines.Dispatchers
 import androidx.room.RoomDatabase
 import androidx.room.Database
-import kotlinx.coroutines.IO
 
 @Database(entities = [NewsEntity::class], version = 1)
 internal abstract class AppDatabase: RoomDatabase() {
