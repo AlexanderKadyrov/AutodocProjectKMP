6f4c4720637f098091c7525ed84f9071ca029901
Alexander K <alexander-kad@yandex.ru>
2026-07-03 00:54:29 +0500

    fixes

--- sharedLogic/src/commonMain/kotlin/com/autodoc/project/services/news/NewsModel.kt
+++ sharedLogic/src/commonMain/kotlin/com/autodoc/project/services/news/NewsModel.kt
@@ -3,9 +3,16 @@ package com.autodoc.project.services.news
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
 
+import androidx.room.PrimaryKey
+import androidx.room.Entity
+
+@Entity
 @Serializable
 data class NewsModel(
+
+    @PrimaryKey
     val id: Int,
+
     val title: String,
 
     @SerialName("description")
