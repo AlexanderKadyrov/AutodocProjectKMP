e042317bd177ad44b86ddebac430d4e3624931be
Alexander K <alexander-kad@yandex.ru>
2026-07-03 01:39:15 +0500

    fixes

---
+++ sharedLogic/src/iosMain/kotlin/com/autodoc/project/di/DI.kt
@@ -0,0 +1,9 @@
+package com.autodoc.project.di
+
+import com.autodoc.project.database.AppDatabase
+import com.autodoc.project.database.getDatabase
+import org.koin.dsl.module
+
+actual fun platformModule() = module {
+    single<AppDatabase> { getDatabase() }
+}
