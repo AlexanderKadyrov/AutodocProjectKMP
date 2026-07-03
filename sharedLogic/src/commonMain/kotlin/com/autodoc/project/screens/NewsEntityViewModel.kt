8b54dacaac78ecbd67c9a2c230a1b2382c17a2c7
Alexander K <alexander-kad@yandex.ru>
2026-07-04 01:51:25 +0500

    fixes

--- sharedLogic/src/commonMain/kotlin/com/autodoc/project/screens/NewsEntityViewModel.kt
+++ sharedLogic/src/commonMain/kotlin/com/autodoc/project/screens/NewsEntityViewModel.kt
@@ -1,10 +1,13 @@
 package com.autodoc.project.screens
 
+import com.rickclephas.kmp.nativecoroutines.NativeCoroutinesState
 import com.rickclephas.kmp.observableviewmodel.ViewModel
 import com.rickclephas.kmp.observableviewmodel.launch
 
 import com.autodoc.project.repositories.NewsRepository
 import com.autodoc.project.services.news.NewsEntity
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -15,7 +18,16 @@ class NewsEntityViewModel(
 
     private val newsRepository: NewsRepository by inject()
 
-    fun addFavorite() {
+    private val _isFavorite = MutableStateFlow(false)
+
+    @NativeCoroutinesState
+    val isFavorite: StateFlow<Boolean>
+        get() {
+            return _isFavorite
+        }
+
+    fun tapFavorite() {
+        _isFavorite.value = !_isFavorite.value
         viewModelScope.launch {
             newsRepository.add(entity)
         }
