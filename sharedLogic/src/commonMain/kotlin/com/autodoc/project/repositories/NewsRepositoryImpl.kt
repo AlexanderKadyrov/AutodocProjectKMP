4bf70a6add6c45f3186342bf8ed308f00ce907be
Alexander K <alexander-kad@yandex.ru>
2026-07-03 19:19:48 +0500

    fixes

--- sharedLogic/src/commonMain/kotlin/com/autodoc/project/repositories/NewsRepositoryImpl.kt
+++ sharedLogic/src/commonMain/kotlin/com/autodoc/project/repositories/NewsRepositoryImpl.kt
@@ -8,16 +8,16 @@ import kotlinx.coroutines.flow.Flow
 
 internal class NewsRepositoryImpl(
     private val database: AppDatabase
-) {
+): NewsRepository {
     private val dao: NewsDao by lazy {
         database.getDao()
     }
 
-    suspend fun add(entity: NewsEntity) {
+    override suspend fun add(entity: NewsEntity) {
         dao.insert(entity)
     }
 
-    fun load(): Flow<List<NewsEntity>> {
+    override fun load(): Flow<List<NewsEntity>> {
         return dao.getAllAsFlow()
     }
 }
