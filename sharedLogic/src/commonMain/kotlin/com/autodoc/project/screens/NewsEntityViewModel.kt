2f8ecc47c511a7a8a523fc09860c0d431dfd2ee7
Alexander K <alexander-kad@yandex.ru>
2026-07-03 19:28:13 +0500

    fixes

--- sharedLogic/src/commonMain/kotlin/com/autodoc/project/screens/NewsEntityViewModel.kt
+++ sharedLogic/src/commonMain/kotlin/com/autodoc/project/screens/NewsEntityViewModel.kt
@@ -1,9 +1,14 @@
 package com.autodoc.project.screens
 
+import com.autodoc.project.repositories.NewsRepository
 import com.autodoc.project.services.news.NewsEntity
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 class NewsEntityViewModel(
     entity: NewsEntity
-) {
+): KoinComponent {
+
+    private val newsRepository: NewsRepository by inject()
 
 }
