416e7127ade8bc844a57119c2fd2fb46f6148ed4
Alexander K <alexander-kad@yandex.ru>
2026-07-05 23:59:06 +0500

    fixes

---
+++ sharedLogic/src/commonMain/kotlin/com/autodoc/project/providers/news/NewsNetworkProvider.kt
@@ -0,0 +1,18 @@
+package com.autodoc.project.providers.news
+
+import com.autodoc.project.services.news.NewsEntity
+import com.autodoc.project.services.news.NewsService
+
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+import kotlin.getValue
+
+class NewsNetworkProvider: KoinComponent {
+
+    private val newsService: NewsService by inject()
+
+    suspend fun fetchNews(offset: Int, limit: Int): List<NewsEntity> {
+        return newsService.fetchNews(offset, limit)
+    }
+}
