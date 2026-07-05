6e64454f54bb10e532e899f643c30d539e38e196
Alexander K <alexander-kad@yandex.ru>
2026-07-06 00:07:07 +0500

    fixes

--- sharedLogic/src/commonMain/kotlin/com/autodoc/project/repositories/NewsRepository.kt
+++ sharedLogic/src/commonMain/kotlin/com/autodoc/project/repositories/NewsRepository.kt
@@ -3,9 +3,10 @@ package com.autodoc.project.repositories
 import com.autodoc.project.services.news.NewsEntity
 import kotlinx.coroutines.flow.Flow
 
-interface NewsRepository {
+internal interface NewsRepository {
     suspend fun add(entity: NewsEntity)
     suspend fun delete(entity: NewsEntity)
-    fun load(): Flow<List<NewsEntity>>
-    fun load(id: Int): Flow<NewsEntity?>
+    fun loadAll(): List<NewsEntity>
+    fun loadAsFlow(): Flow<List<NewsEntity>>
+    fun loadAsFlow(id: Int): Flow<NewsEntity?>
 }
