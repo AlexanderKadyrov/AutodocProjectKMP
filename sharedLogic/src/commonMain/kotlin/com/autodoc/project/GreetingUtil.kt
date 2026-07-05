fffba8a9124e058335a9d0d0ef7bfc4578988d8c
Alexander <alexander-kad@yandex.ru>
2026-07-05 23:28:32 +0500

    Remove files (#14)

--- sharedLogic/src/commonMain/kotlin/com/autodoc/project/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package com.autodoc.project
-
-fun sayHello(to: String): String =
-    "Hello, $to!"
--- sharedLogic/src/commonMain/kotlin/com/autodoc/project/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.autodoc.project
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
